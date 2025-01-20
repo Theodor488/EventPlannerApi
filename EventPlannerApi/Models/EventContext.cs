ffcfca4db96b08daf10997f4cc4c744a34ab57e0
Theodor Suciu <theodorsuciu@yahoo.com>
2025-01-20 09:30:22 -0800

    Added EventRegistration table

--- EventPlannerApi/Models/EventContext.cs
+++ EventPlannerApi/Models/EventContext.cs
@@ -9,4 +9,5 @@ public class EventContext : DbContext
     }
 
     public DbSet<Event> Events { get; set; }
+    public DbSet<EventRegistration> EventRegistrations { get; set; }
 }
---
+++ EventPlannerApi/Models/EventRegistration.cs
@@ -0,0 +1,10 @@
+﻿namespace EventPlannerApi.Models
+{
+    public class EventRegistration
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid EventId { get; set; }
+        public DateTime RegisteredAt { get; set; }
+    }
+}
